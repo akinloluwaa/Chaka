14ed6ba0a8b318fa7d28acd6402be414a455b7ca
akinloluwaa <akinloluwaa@gmail.com>
2021-03-12 13:00:46 +0100

    Adjusted the html file

---
+++ home.js
@@ -0,0 +1,84 @@
+let sector = [
+ {
+  name: "Health Care",
+  percentage: "+0.46%",
+  image: "./images/",
+ },
+ {
+  name: "Communication Service",
+  percentage: "0.00%",
+  imgFile: "1.jpg",
+ },
+ {
+  name: "Utilities",
+  percentage: "-0.07%",
+  imgURL: "",
+ },
+ {
+  name: "Consumer Discretionary",
+  percentage: "-0.07%",
+  image: "./images/",
+ },
+ {
+  name: "Finacials",
+  percentage: "-0.33%",
+  image: "./images/",
+ },
+ {
+  name: "Consumer Staples",
+  percentage: "-0.38%",
+  image: "./images/",
+ },
+ {
+  name: "Materials",
+  percentage: "-0.42%",
+  image: "./images/",
+ },
+ {
+  name: "Technology",
+  percentage: "-0.51%",
+  image: "./images/",
+ },
+ {
+  name: "Real Estate",
+  percentage: "-0.55%",
+  image: "./images/",
+ },
+ {
+  name: "Energy",
+  percentage: "-0.55%",
+  image: "./images/",
+ },
+ {
+  name: "Industrials",
+  percentage: "-0.78%",
+  image: "./images/",
+ },
+];
+
+for(i = 0; i <= sector.length; i++) {
+    console.log(sector)
+}
+
+// //function div() {
+// var contain = document.querySelector(".content");
+// let background = "";
+// let name = "";
+// let percentage = "";
+// let template =
+//  '<div class="secs" style=background:"' +
+//  background +
+//  '"><div class="description"><h3>' +
+//  name +
+//  "</h3>" +
+//  '</div><div class="stat"><p>' +
+//  percentage +
+//  "</p></div></div>";
+
+// sector.forEach((e) => {
+//  background = e.image;
+//  name = e.name;
+//  percentage = e.percentage;
+//  contain.innerHTML(template);
+// });
+// //}
