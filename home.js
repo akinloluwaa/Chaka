2a935c411b6c89fe34bc6dbf08a27bd586437575
akinloluwaa <akinloluwaa@gmail.com>
2021-03-13 13:34:36 +0100

    Changed file

--- home.js
+++ home.js
@@ -1,125 +1,63 @@
 let sector = [
  {
   name: "Health Care",
-  percentage: "+0.46",
-  image: "url('./images/1.jpg')",
+  percentage: "+0.46%",
+  image: "./images/",
  },
  {
   name: "Communication Service",
-  percentage: "0.00",
-  image: "url('./images/2.jpg')",
+  percentage: "0.00%",
+  imgFile: "1.jpg",
  },
  {
   name: "Utilities",
-  percentage: "-0.07",
-  image: "url('./images/3.jpg')",
+  percentage: "-0.07%",
+  imgURL: "",
  },
  {
   name: "Consumer Discretionary",
-  percentage: "-0.07",
-  image: "url('./images/4.jpg')",
+  percentage: "-0.07%",
+  image: "./images/",
  },
  {
   name: "Finacials",
-  percentage: "-0.33",
-  image: "url('./images/5.jpg')",
+  percentage: "-0.33%",
+  image: "./images/",
  },
  {
   name: "Consumer Staples",
-  percentage: "-0.38",
-  image: "url('./images/6.jpg')",
+  percentage: "-0.38%",
+  image: "./images/",
  },
  {
   name: "Materials",
-  percentage: "-0.42",
-  image: "url('./images/7.jpg')",
+  percentage: "-0.42%",
+  image: "./images/",
  },
  {
   name: "Technology",
-  percentage: "-0.51",
-  image: "url('./images/8.jpg')",
+  percentage: "-0.51%",
+  image: "./images/",
  },
  {
   name: "Real Estate",
-  percentage: "-0.55",
-  image: "url('./images/9.jpg')",
+  percentage: "-0.55%",
+  image: "./images/",
  },
  {
   name: "Energy",
-  percentage: "-0.55",
-  image: "url('./images/10.jpg')",
+  percentage: "-0.55%",
+  image: "./images/",
  },
  {
   name: "Industrials",
-  percentage: "-0.78",
-  image: "url('./images/11.jpg')",
+  percentage: "-0.78%",
+  image: "./images/",
  },
 ];
 
-
-function createCard () {
-    for (i = 0; i < sector.length; i++) {
-        let sectors = document.querySelector('.content')
-        let cards = document.createElement('div');
-        cards.className = 'secs';
-        cards.style.backgroundImage = sector[i].image;
-        let title = document.createElement('div');
-        title.className = 'description';
-        let head = document.createElement('h3');
-        head.innerHTML = sector[i].name;
-        title.appendChild(head);
-        let rate = document.createElement('div');
-        rate.className = 'stat';
-        let rating = document.createElement('p');
-        rating.innerHTML = sector[i].percentage + "%";
-        if (sector[i].percentage < 0) {
-            rating.style.backgroundColor = "red"
-        } else if (sector[i].percentage > 0) {
-            rating.style.backgroundColor = "green";
-        } else {
-            rating.style.backgroundColor = "grey";
-        };
-        title.appendChild(head);
-        rate.appendChild(rating);
-        cards.appendChild(title);
-        cards.appendChild(rate);
-        sectors.appendChild(cards);
-    }
+for(i = 0; i <= sector.length; i++) {
+    console.log(sector)
 }
-
-createCard();
-
-function createSectors () {
-    for (i = 0; i < 2; i++) {
-        let sectors = document.querySelector('.sectors')
-        let cards = document.createElement('div');
-        cards.className = 'sec';
-        cards.style.backgroundImage = sector[i].image;
-        let title = document.createElement('div');
-        title.className = 'description';
-        let head = document.createElement('h3');
-        head.innerHTML = sector[i].name;
-        title.appendChild(head);
-        let rate = document.createElement('div');
-        rate.className = 'stat';
-        let rating = document.createElement('p');
-        rating.innerHTML = sector[i].percentage + '%';
-        if (sector[i].percentage < 0) {
-            rating.style.backgroundColor = "red"
-        } else if (sector[i].percentage > 0) {
-            rating.style.backgroundColor = "green";
-        } else {
-            rating.style.backgroundColor = "grey";
-        };
-        title.appendChild(head);
-        rate.appendChild(rating);
-        cards.appendChild(title);
-        cards.appendChild(rate);
-        sectors.appendChild(cards);
-    }
-}
-
-createSectors();
-
 
 
